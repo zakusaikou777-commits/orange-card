05e6e5700a604bc8f4ab22de7813489f79e6f49f
zakusaikou777-commits <zakusaikou777@gmail.com>
2026-09-06 14:39:02 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Orange Card v16 - オフラインで開けるようにするためのキャッシュ
-const CACHE = "orange-card-v16";
+// Orange Card v16.4 - オフラインで開けるようにするためのキャッシュ
+const CACHE = "orange-card-v16.4";   // 版ごとに変える（変え忘れると古い画面が残る）
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
